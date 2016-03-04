927985ab7f3380b2a7f22698de69a33da273507b
Bhargava Manja <bhargava.manja@runtimeverification.com>
2016-03-03 18:27:16 -0600

    Have all of what Joe requested, layout might be janky

--- annotator.py
+++ annotator.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+## TODO
+# * move to relative layout
 
 import kivy
 kivy.require('1.9.0')
@@ -23,7 +25,8 @@ from kivy.core.window import Window
 import sys
 from optparse import OptionParser
 from glob import glob
-from os.path import join
+from os.path import join, exists
+import json
 
 # for saving
 import json
@@ -54,63 +57,80 @@ class AnnotatorApp(App):
         box.add_widget(image)
         box.add_widget(label)
         self.root.add_widget(box)
-        self.root.add_widget(ImageDisplay())
+        imd = ImageDisplay()
+        self.root.add_widget(imd)
         self.icon = 'hand-icon.png'
 
 class ImageDisplay(Widget):
-    _instructions_text = ("Create a bounding box around the depth image to closely "
-                          "bound the object, ignoring noise. Bounding boxes must be "
-                         "created with a smooth movement. To delete a bad box, "
-                         "simply click anywhere else in the depth image. After "
-                         "selecting a grip, hit the right and left arrow keys to move on or see")
-
+    _instructions_text = ("After "
+                         "selecting a grip, hit the right and left arrow keys to move on or see an old image")
 
     def __init__(self, **kwargs):
         self.image_list = self._get_files()
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
         self.im_idx = 0
-        self.annotations = dict()
+
+        old_annotation_fname = join(options.output_dir, "annotations.json")
+        if exists(old_annotation_fname):
+            with open(old_annotation_fname, 'r') as old_annotations:
+                self.annotations = json.load(old_annotations)
+        else:
+            self.annotations = dict()
+
         super(ImageDisplay, self).__init__(**kwargs)
+        self._load_old_annotation()
 
     def _get_files(self):
-        filenames = sorted(glob(join(options.imdir, '*.jpg')))
+        filenames = sorted(glob(join(options.imdir, '*.png')))
         return filenames
 
     def _keyboard_closed(self):
-        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
-        self._keyboard = None
+        pass
+        #self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        #self._keyboard = None
 
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        # only deal with arrow keys
+        if keycode[1] != "left" and keycode[1] != "right":
+            return
+
         # don't walk off either end of list of images
         if self.im_idx == 0 and keycode[1] == 'left':
             return
-        elif self.im_idx == (len(self.image_list)/3)-1 and keycode[1] == 'right':
+        elif self.im_idx == len(self.image_list)-1 and keycode[1] == 'right':
             return
 
+        # save current annotations
+        self.record_annotation()
+        self.save_annotations()
+
+        # change index
         if keycode[1] == 'left':
             self.im_idx -= 1
         elif keycode[1] == 'right':
             self.im_idx += 1
 
-        self.record_annotation()
-        self.ids.left_image.source = self.left_image()
-        self.ids.right_image.source = self.right_image()
-        self.ids.depth_image.source = self.depth_image()
-        self.ids.depth_image.clear_line()
+        # clear annotations from GUI
+        self.ids.comments.text = ""
+        self.ids.power.active = False
+        self.ids.tool.active = False
+        self.ids.tjc.active = False
+        self.ids.pinch.active = False
+        self.ids.key.active = False
 
-    def left_image(self):
-        return self.image_list[(self.im_idx*3)+1]
-    def right_image(self):
-        return self.image_list[(self.im_idx*3)+2]
-    def depth_image(self):
-        return self.image_list[self.im_idx*3]
+        # if previously annotated, load old annotations
+        self._load_old_annotation()
+        # display new image
+        self.ids.curr_image.source = self.curr_image()
+
+    def curr_image(self):
+        return self.image_list[self.im_idx]
 
     def record_annotation(self):
-        curr_left_im = self.left_image()
-        handle = curr_left_im[len(options.imdir):curr_left_im.rfind('_')]
-        self.annotations[handle] = dict()
-
+        handle = self._get_handle()
+        if handle not in self.annotations.keys():
+            self.annotations[handle] = dict()
         grip = 'None'
         if self.ids.power.active:
             grip = 'power'
@@ -125,42 +145,37 @@ class ImageDisplay(Widget):
         self.annotations[handle]['grip'] = grip
 
         self.annotations[handle]['comment'] = self.ids.comments.text
-        repeated_points = [iter(self.ids.depth_image.line_disp.points)] * 2
-        grouped_points = izip_longest(fillvalue=None, *repeated_points)
-        (xo, yo) = self.ids.depth_image.pos
-        self.annotations[handle]['line'] = map(lambda (x,y): (x-xo, y-yo),
-                                               grouped_points)
 
     def save_annotations(self):
         outfile_name = options.output_dir + 'annotations.json'
         with open(outfile_name, 'w') as outfile:
             json.dump(self.annotations, outfile)
 
-class BorderDrawer(Image):
-    # TODO compute local line coords for matlab
-    def __init__(self, **kwargs):
-        super(BorderDrawer, self).__init__(**kwargs)
-        self.line_disp = Line()
+    def _get_handle(self):
+        im_name = self.curr_image()
+        handle = im_name[im_name.rfind('/') + 1:]
+        return handle
 
-    def on_touch_down(self, touch):
-        if not self.collide_point(*touch.pos):
-            return
-        self.clear_line()
-        self.canvas.add(self.line_disp)
-
-    def on_touch_move(self, touch):
-        if not self.collide_point(*touch.pos):
-            return
-        self.line_disp.points += [touch.x, touch.y]
-
-    def clear_line(self):
-        self.line_disp.points = []
-
+    def _load_old_annotation(self):
+        handle = self._get_handle()
+        if handle in self.annotations.keys():
+            self.ids.comments.text = self.annotations[handle]['comment']
+            old_grip = self.annotations[handle]['grip']
+            if old_grip == 'power':
+                self.ids.power.active = True
+            elif old_grip == 'tool':
+                self.ids.tool.active = True
+            elif old_grip == '3 jaw chuck':
+                self.ids.tjc.active = True
+            elif old_grip == 'pinch':
+                self.ids.pinch.active = True
+            elif old_grip == 'key':
+                self.ids.key.active = True
 
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option('--imdir', '-i', dest='imdir',
-            help='location of all duo left/right and depth map images')
+            help='location of images')
     parser.add_option('--output-dir', '-o', dest='output_dir',
             help='location where app output files should go')
 
